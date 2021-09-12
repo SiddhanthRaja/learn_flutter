19ba355167317df50982b266ac910d3991467cde
SiddhanthRaja <siddunishtu@gmail.com>
2021-09-12 13:00:25 +0530

    With scorecard

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,20 +3,24 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
 
   final Function pointer;
+  final String option;
 
-  Answer(this.pointer);
+  Answer(this.option,this.pointer);
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle custom = ElevatedButton.styleFrom(onPrimary: Colors.white, textStyle: TextStyle(fontSize: 25.0),shadowColor: Colors.pinkAccent);
     return Container(
-      width: 18.0 ,
+      width: double.infinity ,
       child:
       ElevatedButton(
-        onPressed: () {
-          print("I am op1");
-          pointer;
-        },
-        child: Text("Opt 1"),
+
+          child: Text(option),
+          style: custom,
+          onPressed: () => pointer(),
+
+
+
       ),
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -16,34 +16,50 @@ class SidApp extends StatefulWidget {
 
 
 class _SidAppState extends State<SidApp>{
-  var _quesList = ["Q100","Q101","Q103"];
+  var _quesList = [
+    {"quesText": "Q100", "options" : [
+        {"opt":"A", "score":20},{"opt":"B", "score":10},{"opt":"C", "score":5},{"opt":"D", "score":2}]  },
+    {"quesText": "Q101", "options" : [
+      {"opt":"X", "score":20},{"opt":"Y", "score":10},{"opt":"Z", "score":5},{"opt":"W", "score":2}]  },
+    {"quesText": "Q102", "options" : [
+      {"opt":"1", "score":20},{"opt":"2", "score":10},{"opt":"3", "score":5},{"opt":"4", "score":2}]  },
+  ];
 
   var _index = 0;
+  var _total = 0;
 
 
-
-  void changeQ(){
-    setState((){_index = _index+1; });
+  changeQ(int score){
+    setState(()
+    {_index = _index+1;
+    _total = _total + score;
+    });
     print("I am Q" + _index.toString());
   }
 
   Widget build(BuildContext context) {
     // TODO: implement build
+    final List<Map> listOp = _quesList[_index]["options"] as List<Map>;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Sid's App",style: TextStyle(color: Colors.deepOrange, backgroundColor: Colors.greenAccent) ),),
-        body: Column(
-            children: [
-            Question(_quesList[_index], ),
-            Answer(changeQ),
-            Answer(changeQ),
-            Answer(changeQ),
+        body: _index<= (_quesList.length -2) ? Column(
+          children: [
+
+            Question(_quesList[_index]["quesText"] as String ),
+
+            Answer(listOp[0]["opt"],() => changeQ(listOp[0]["score"]) ,),
+            Answer(listOp[1]["opt"],() => changeQ(listOp[1]["score"]) ,),
+            Answer(listOp[2]["opt"],() => changeQ(listOp[2]["score"]) ,),
+            Answer(listOp[3]["opt"],() => {_total += (listOp[3]["score"]) as int }),
 
 
-            ]),
+            ]) : Center(child: Text("You have completed the Quiz\n\n" + "Your score is $_total"),)
+
       ),
 
 
     );
+
   }
 }
